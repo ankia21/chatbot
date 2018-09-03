cc42210c3fb020050119e53b3f6512517f042f99
Alonso <Alonso@iMac-de-Alonso.local>
2018-09-02 21:57:20 -0500

    additional changes

--- routes/api.js
+++ routes/api.js
@@ -49,6 +49,69 @@ router.post('/', (req, res) => {
 });
 
 function receivedMessage(event){
-  console.log("Message Data:", event.message);
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",senderID,recipientID,timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+  var messageText= message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText){
+
+    // If we received a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received
+    switch(messageText){
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments){
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendGenericMessage(recipientId){
+
+}
+function sendTextMessage(recipientId, messageText){
+  var messageData = {
+    recipient : {
+      id: recipientId
+    },
+    message : {
+      text: messageText
+    }
+  };
+  callSendAPI(messageData);
+
+}
+
+function callSendAPI(messageData){
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: accessToken},
+    method: 'POST',
+    json: messageData
+  
+  }, function (error, response, body){
+
+    if (!error && response.statusCode == 200){
+      var recipientId = body.recipient_id;
+      var messageId   = body.message_id;
+      
+      console.log("Successfully send generic message with id %s to recipient %s",messageId,recipientId);
+    } else {
+      console.error("unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
 }
 module.exports = router;
